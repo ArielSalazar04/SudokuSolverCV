39450c6c687f06db46dbedf50365775eb5f06e57
ArielSalazar <arielsalazarraw@gmail.com>
2021-11-11 08:06:13 -0500

    Create GUI

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-import cv2
+from App import App
 
-vc = cv2.VideoCapture(0)
-
-while True:
-    # Read a frame
-    success, img = vc.read()
-
-    # Show the frame on preview
-    cv2.imshow("Sudoku Solver with Computer Vision", img)
-
-    # Break loop if 'q' key is pressed
-    if cv2.waitKey(1) & 0xff == ord('q'):  # exit on q
-        cv2.destroyAllWindows()
-        break
+myApp = App()
